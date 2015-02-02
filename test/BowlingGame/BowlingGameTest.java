7c5bb2ecc8b1c9d72a5c1e150fb70b87fa4d53af
tsmienk <tim.smienk@gmail.com>
2015-02-02 19:51:12 +0100

    Added test for spare
    also swapped arguments for assertEquals. Expected value is first
    argument, actual value second.

--- test/BowlingGame/BowlingGameTest.java
+++ test/BowlingGame/BowlingGameTest.java
@@ -9,7 +9,7 @@ public class BowlingGameTest
   public void testNewGame()
   {
     BowlingGame bg = new BowlingGame();    
-    assertEquals(bg.getTotalScore(), 0);
+    assertEquals(0, bg.getTotalScore());
   }
   
   @Test
@@ -17,7 +17,7 @@ public class BowlingGameTest
   {
     BowlingGame bg = new BowlingGame();    
     bg.score(0);
-    assertEquals(bg.getTotalScore(), 0);
+    assertEquals(0, bg.getTotalScore());
   }
   
   @Test
@@ -25,7 +25,7 @@ public class BowlingGameTest
   {
     BowlingGame bg = new BowlingGame();    
     bg.score(1);
-    assertEquals(bg.getTotalScore(), 1);
+    assertEquals(1, bg.getTotalScore());
   }
   
   @Test
@@ -34,6 +34,16 @@ public class BowlingGameTest
     BowlingGame bg = new BowlingGame();    
     bg.score(1);
     bg.score(1);
-    assertEquals(bg.getTotalScore(), 2); 
+    assertEquals(2, bg.getTotalScore()); 
+  }
+  
+  @Test
+  public void testSpare()
+  {
+    BowlingGame bg = new BowlingGame();
+    bg.score(5);
+    bg.score(5);
+    bg.score(5);
+    assertEquals(20, bg.getTotalScore()); 
   }
 }
